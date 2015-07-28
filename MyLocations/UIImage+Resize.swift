ac831d7c3244f8714259ac9c6b7da5ee85fafb15
snake <lib0707@gmail.com>
2015-07-28 18:34:50 +0800

    06 – Photo Picker

---
+++ MyLocations/UIImage+Resize.swift
@@ -0,0 +1,25 @@
+//
+//  UIImage+Resize.swift
+//  MyLocations
+//
+//  Created by snake on 15/7/28.
+//  Copyright (c) 2015年 snakeLib. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    func resizedImageWithBounds(bounds: CGSize) -> UIImage {
+        let horizontalRatio = bounds.width / size.width
+        let verticalRatio = bounds.height / size.height
+        let ratio = min(horizontalRatio, verticalRatio)
+        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
+        drawInRect(CGRect(origin: CGPoint.zeroPoint, size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
